9b36a501a6c31e3759e04de8f50b30484933ffe0
Alonso Gonzalez <agonzalezmtz@google.com>
2026-03-07 20:43:52 -0600

    Send new changes both

--- environments/gcp-env-demo/k8s-apps/gen-k8s-outputs.tf
+++ environments/gcp-env-demo/k8s-apps/gen-k8s-outputs.tf
@@ -1,13 +1,1 @@
 # --- GKE(K8S) LOCUST and GRAFANA ENDPOINTS ---
-
-# --- LOCUST (Load Testing) ---
-output "locust_web_ui" {
-  description = "Public URL for Locust Web Interface"
-  value       = "http://${kubernetes_service.locust_master_public.status.0.load_balancer.0.ingress.0.ip}:8089"
-}
-
-# --- GRAFANA (Monitoring) ---
-output "grafana_web_url" {
-  description = "Public URL for Grafana Dashboard"
-  value       = "http://${kubernetes_service.grafana_web.status.0.load_balancer.0.ingress.0.ip}"
-}
