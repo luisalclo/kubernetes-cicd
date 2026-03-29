4de426b166515503224683eb2d0a812e75364716
Alonso Gonzalez <agonzalezmtz@google.com>
2026-03-29 17:59:57 -0600

    fix: remove undeclared subnet outputs from vpc module

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -9,22 +9,10 @@ output "network_self_link" {
   value       = var.vpc.create ? google_compute_network.network[0].self_link : null
 }
 
-# Output for Subnet 01 Name
-output "subnet_01_name" {
-  description = "The name of the first subnetwork created."
-  value       = var.vpc.create ? google_compute_subnetwork.subnet_01[0].name : null
-}
-
 # Output for Subnet 02 Name
 output "subnet_02_name" {
   description = "The name of the second subnetwork created."
   value       = var.vpc.create ? google_compute_subnetwork.subnet_02[0].name : null
-}
-
-# Output for Subnet 03 Name (New)
-output "subnet_03_name" {
-  description = "The name of the third subnetwork created (Load Balancers)."
-  value       = var.vpc.create ? google_compute_subnetwork.subnet_03[0].name : null
 }
 
 # Output for the Project ID
